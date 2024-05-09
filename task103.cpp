1396ed082dc0d8d913d1b9f0c52def874116a0a2
Tareq-Monowar <tareq31072003@gamil.com>
2024-05-09 19:57:57 +0600

    Changed Circle Speed

--- task103.cpp
+++ task103.cpp
@@ -6,7 +6,7 @@
 #define SCREEN_WIDTH 500
 #define SCREEN_HEIGHT 500
 #define CIRCLE_RADIUS 20
-#define CIRCLE_SPEED 5
+#define CIRCLE_SPEED 8
 
 bool initializeSDL(SDL_Window** window, SDL_Renderer** renderer) {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
